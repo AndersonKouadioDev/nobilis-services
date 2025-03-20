bf77cffae130951f5fca64176afe1266b2735d21
nandju <nlbmd17@gmail.com>
2025-03-20 15:31:48 +0000

    reste trois partie pour le one

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Client } from '@/components/home/parteners/client';
 import Hero from '../components/home/hero/hero';
 import Propos from '../components/home/propos/propos';
 import Service from '../components/home/service/service';
+import Blog from '../components/home/blog/blog';
 
 
 
@@ -15,6 +16,7 @@ export default function Home() {
       <Client/>
       <Propos/>
       <Service/>
+      <Blog/>
     </div>
   );
 }
--- components/home/blog/blog.tsx
+++ components/home/blog/blog.tsx
@@ -1,102 +1,64 @@
+import { Button } from '@nextui-org/react';
 import Image from 'next/image';
 
 
 export default function Blog(){
 
     type News = {
-        title: string;
-        substitle: string;
         description: string;
-        profil: string;
         image: string;
         num: string;
-        month: string;
       };
 
       const news: News[] = [
         {
-          title: 'Kristin Watson',
-          substitle: 'President de Clean Service',
           description:
-            'Comment maintenir une cuisine impeccable',
-          profil: '/assets/images/illustrations/page-accueil/profil1.png',
-          image: '/assets/images/illustrations/page-accueil/card_1.png',
-          num: '22',
-          month: 'AVR',
+            'Lorem ipsum dolor sit amecon sectetur adipisicing elit, sed do eiusmod tempor',
+          image: '/assets/images/illustrations/page-accueil/image.png',
+          num: '10.03.2024',
         },
         {
-          title: 'Ralph Edwards',
-          substitle: 'President de Clean Service',
           description:
-            "Les secrets d'un bureau toujours propre",
-          profil: '/assets/images/illustrations/page-accueil/profil2.png',
-          image: '/assets/images/illustrations/page-accueil/card_2.png',
-          num: '16',
-          month: 'JUI',
+            "Lorem ipsum dolor sit amecon sectetur adipisicing elit, sed do eiusmod tempor",
+          image: '/assets/images/illustrations/page-accueil/image-1.png',
+          num: '10.03.2025',
         },
         {
-          title: 'Albert Flores',
-          substitle: 'President de Clean Service',
           description:
-            'Nettoyage écologique : nos astuces',
-          profil: '/assets/images/illustrations/page-accueil/profil3.png',
-          image: '/assets/images/illustrations/page-accueil/card_3.png',
-          num: '31',
-          month: 'DEC',
-        },
-        {
-          title: 'Jacob Jones',
-          substitle: 'President de Clean Service',
-          description:
-            'Nettoyage écologique : nos astuces',
-          profil: '/assets/images/illustrations/page-accueil/profil4.png',
-          image: '/assets/images/illustrations/page-accueil/card_4.png',
-          num: '18',
-          month: 'MAI',
+            'Lorem ipsum dolor sit amecon sectetur adipisicing elit, sed do eiusmod tempor',
+          image: '/assets/images/illustrations/page-accueil/div.service-img-2.png',
+          num: '10.03.2026',
         },
         
       ]
 
     return (
-        <div className="bg-white p-10">
+        <div className="bg-[#E6EEF5] p-10">
             <div className="flex flex-col gap-6 py-20 items-center">
-                <div className="text-primary text-lg font-bold">DERNIÈRES NOUVELLES</div>
-                <div className="text-primary-600 text-2xl font-black">LISEZ NOTRE DERNIER BLOG</div>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 py-6">
+                <div className="text-primary text-2xl font-extrabold">Blog</div>
+                <div className="text-center text-muted font-semibold">Nous sommes passionner de travailler avec vous</div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-12">
                   {news.map((item) => (
-                    <div key={item.title} className="flex flex-col gap-4 font-worksans border-2 rounded-lg h-full">
+                    <div key={item.num} className="flex flex-col gap-4 font-worksans border-2 rounded-2xl bg-white h-full">
                       <div className="relative">
                         <Image
-                          className="w-full h-auto object-cover aspect-[16/9]"
+                          className="w-full h-auto object-cover object-center aspect-[16/9] rounded-t-2xl"
                           src={item.image}
-                          alt={item.title}
+                          alt={item.num}
                           width={200}
                           height={150}
                           priority
                         />
-                        <div className="absolute right-4 -bottom-8 flex flex-col gap-1 text-white bg-black p-2 rounded-lg items-center w-16">
+                        <div className="absolute left-4 -bottom-8 flex flex-col gap-1 text-white bg-primary px-8 py-2 rounded-lg items-center w-24">
                           <div>{item.num}</div>
-                          <div>{item.month}</div>
                         </div>
                       </div>
-                      <div className="flex flex-row gap-4 items-center px-4">
-                        <Image
-                          className="w-10 h-10 object-contain"
-                          src={item.profil}
-                          alt={item.title}
-                          width={30}
-                          height={30}
-                          priority
-                        />
-                        <div className="flex flex-col">
-                          <div>{item.title}</div>
-                          <div className="text-gray-500">{item.substitle}</div>
-                        </div>
-                      </div>
-                      <div className="text-gray-500 p-6 text-start">{item.description}</div>
+                      
+                      <div className="text-black p-6 text-start">{item.description}</div>
                     </div>
                   ))}
                 </div>
+                <Button className="bg-muted text-white" radius='full'>Voir plus d'article</Button>
             </div>
         </div>
     )
